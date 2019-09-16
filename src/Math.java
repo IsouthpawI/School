cff68467ff4bca3668d95fd532406852dbcf995a
IsouthpawI <iiisouthpawiii@gmail.com>
2019-09-16 13:21:44 +0200

    new2

--- src/Math.java
+++ src/Math.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Math {
+
+    public static void main(String[] args) {
+
+        System.out.println("Type Int");
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine();
+        char[]arr =a.toCharArray();
+
+        for(int i=arr.length-1; i>=0; i--){
+            System.out.printf("%s", arr[i]);
+        }
+        System.out.println();
+    }
 }
