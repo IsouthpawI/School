50ef9d57bbeb73ecaea4fb1ec00ca840a16f5fcf
IsouthpawI <iiisouthpawiii@gmail.com>
2019-09-16 13:16:58 +0200

    new

---
+++ src/English.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class English {
+
+    public static void main(String[] args) {
+        new English().run();
+    }
+
+    public void run()
+    {
+
+        System.out.println("Input String:\n");////
+        Scanner keyboardScanner = new Scanner(System.in);/////
+        String inString = keyboardScanner.nextLine();/////
+        String shortMessage = shortifyMessage(inString);
+        System.out.println(shortMessage);
+    }
+
+    public String shortifyMessage(String str)
+    {
+        String s = str;
+        s = s.replace("Pes", "Dog");
+        s = s.replace("Macka", "Cat");
+        s = s.replace("Vtak", "Bird");
+        s = s.replace("Ryba", "Fish");
+        s = s.replace("Kajman", "Aligator");
+
+        return s;
+    }
+
+}
