38154b31bd01788782a7e29801e2d1055a15fe49
IsouthpawI <iiisouthpawiii@gmail.com>
2019-09-16 13:25:00 +0200

    new3

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,39 +1,6 @@
 package Main;
 
-import java.util.Scanner;
-
 public class Main {
-
-    public static void main(String[] args) {
-
-        System.out.println("Type Text");
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        String k;
-        String []s1 = s.split("\\.");
-
-        for(int i=0;i<s1.length;i++)
-        {
-            String []mass = s1[i].split("\\ ");
-
-            {
-                for(int j=0;j<mass.length;j++){
-
-                    {
-                        if(j!=0) {
-                            k = mass[j].toLowerCase();
-                            System.out.print(k + " ");
-                        }
-                        else
-                            System.out.print(mass[j]+" ");
-                    }
-
-                }
-                System.out.print(".");
-            }
-        }
-
-    }
 
 }
 
